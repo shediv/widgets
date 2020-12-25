bfb3481194f0576897a8ae7ae4f33506e196979b
shediv <videshhedge@gmail.com>
2020-12-25 23:14:20 +0530

    Add files via upload

--- function.js
+++ function.js
@@ -3,7 +3,7 @@
 document.getElementById("loan-form").addEventListener("submit", function(e) {
   // Hide Results
   document.getElementById("result").style.display = "none";
-  document.getElementById("bowling_overs").style.display = "none";
+  // document.getElementById("bowling_overs").style.display = "none";
 
   // Show Loader
   document.getElementById("loading").style.display = "block";
@@ -17,11 +17,11 @@ document.getElementById("loan-form").addEventListener("submit", function(e) {
 function calculateResults() {
   
   let matches_count = parseInt(document.getElementById("matches_count").value);
-  let overs_per_match_count = parseInt(document.getElementById("overs_per_match_count").value);
+  // let overs_per_match_count = parseInt(document.getElementById("overs_per_match_count").value);
   let run_scored = parseInt(document.getElementById("run_scored").value);
   let overs_played = parseFloat(document.getElementById("overs_played").value);
   let run_given = parseInt(document.getElementById("run_given").value);
-  let overs_bowled = document.getElementById("overs_bowled");
+  let overs_bowled = parseFloat(document.getElementById("overs_bowled").value);
   let batting_nrr = document.getElementById("batting_nrr");
   let bowling_nrr = document.getElementById("bowling_nrr");
   let nrr = document.getElementById("nrr");
@@ -32,24 +32,26 @@ function calculateResults() {
     let exactOversPlayed = parseInt(overs_playedByTeam[0]);
     let exactBallsPlayed = overs_playedByTeam[1] ? parseInt(overs_playedByTeam[1]) : 0;
     let totalOversPlayed = (exactOversPlayed + (0.16 * exactBallsPlayed));
-
     let battingNRR = (run_scored/totalOversPlayed);
 
     // Bowling
-    let bowlingNRR = (run_given/(matches_count*overs_per_match_count));
+    let overs_bowledByTeam = (overs_bowled + "").split(".");
+    let exactOversBowled = parseInt(overs_bowledByTeam[0]);
+    let exactBallsBowled = overs_bowledByTeam[1] ? parseInt(overs_bowledByTeam[1]) : 0;
+    let totalOversBowled = (exactOversBowled + (0.16 * exactBallsBowled));
+    let bowlingNRR = (run_given/totalOversBowled);
 
     // nrr
     let totalNRR = battingNRR - bowlingNRR;
 
     // Set NRR values
-    overs_bowled.value = matches_count*overs_per_match_count;
     batting_nrr.value = battingNRR;
     bowling_nrr.value = bowlingNRR;
     nrr.value = totalNRR;    
 
     // Show Results
     document.getElementById("result").style.display = "block";
-    document.getElementById("bowling_overs").style.display = "block";
+    // document.getElementById("bowling_overs").style.display = "block";
 
     // Hide Loader
     document.getElementById("loading").style.display = "none";
@@ -62,7 +64,7 @@ function calculateResults() {
 function showError(error) {
   // Hide Results
   document.getElementById("result").style.display = "none";
-  document.getElementById("bowling_overs").style.display = "none";
+  // document.getElementById("bowling_overs").style.display = "none";
 
   // Hide Loader
   document.getElementById("loading").style.display = "none";
